0556ca5d4983a678d7bfa25acb0729414d167a07
djias-23 <1184048208@qq.com>
2026-09-05 11:21:15 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+  <van-tabbar v-if="['home', 'chat', 'user'].includes(route.name)" route >
+    <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item to="/chat" icon="chat-o">对话</van-tabbar-item>
+    <van-tabbar-item to="/user" icon="user-o">用户</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const count = ref(0)
+</script>
